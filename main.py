f3919d397267b258c34846d463f87d33cad33bc3
klove2020 <abczj2020@gmail.com>
2021-08-04 14:32:30 +0800

    update file

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ sys.path.append("src")
 from run import main
                     
 if __name__ == "__main__":
-    main(target_user_id = 518766, option=0, num = 15)
+    main(target_user_id = 518766, option=1, num = 15)
